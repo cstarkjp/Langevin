90c1ed3c5b3befc05d5c078970dbd2cf6b5ff264
cstarkjp <cstarkjp@gmail.com>
2025-11-05 10:05:43 +0800

    Restructure C++ into parallel subfolders

--- site/doxygen/dir_413eba86a22d58dd6c01dd4edd69cedc.js
+++ site/doxygen/dir_413eba86a22d58dd6c01dd4edd69cedc.js
@@ -1,18 +1,5 @@
 var dir_413eba86a22d58dd6c01dd4edd69cedc =
 [
-    [ "dp", "dir_29b47bf0c8dc04d64d9bcc2119390c05.html", "dir_29b47bf0c8dc04d64d9bcc2119390c05" ],
-    [ "langevin_base.hpp", "langevin__base_8hpp.html", "langevin__base_8hpp" ],
-    [ "langevin_bc.cpp", "langevin__bc_8cpp.html", null ],
-    [ "langevin_coefficients.hpp", "langevin__coefficients_8hpp.html", "langevin__coefficients_8hpp" ],
-    [ "langevin_construct_grid.cpp", "langevin__construct__grid_8cpp.html", null ],
-    [ "langevin_construct_grid1d.cpp", "langevin__construct__grid1d_8cpp.html", null ],
-    [ "langevin_construct_grid2d.cpp", "langevin__construct__grid2d_8cpp.html", null ],
-    [ "langevin_enums.hpp", "langevin__enums_8hpp.html", "langevin__enums_8hpp" ],
-    [ "langevin_ic.cpp", "langevin__ic_8cpp.html", null ],
-    [ "langevin_integrate_euler.cpp", "langevin__integrate__euler_8cpp.html", null ],
-    [ "langevin_integrate_rungekutta.cpp", "langevin__integrate__rungekutta_8cpp.html", null ],
-    [ "langevin_parameters.hpp", "langevin__parameters_8hpp.html", "langevin__parameters_8hpp" ],
-    [ "langevin_prepare.cpp", "langevin__prepare_8cpp.html", null ],
-    [ "langevin_types.hpp", "langevin__types_8hpp.html", "langevin__types_8hpp" ],
-    [ "langevin_utilities.cpp", "langevin__utilities_8cpp.html", null ]
+    [ "core", "dir_c0fa894c46f40ef762491bc95fc794ce.html", "dir_c0fa894c46f40ef762491bc95fc794ce" ],
+    [ "dp", "dir_29b47bf0c8dc04d64d9bcc2119390c05.html", "dir_29b47bf0c8dc04d64d9bcc2119390c05" ]
 ];
