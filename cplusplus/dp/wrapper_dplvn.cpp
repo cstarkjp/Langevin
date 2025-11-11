e729122a0bbe001ddff6a5976919d01e9969a5ea
cstarkjp <cstarkjp@gmail.com>
2025-11-11 20:47:03 +0800

    Update version #

--- cplusplus/dp/wrapper_dplvn.cpp
+++ cplusplus/dp/wrapper_dplvn.cpp
@@ -25,7 +25,7 @@
  */
 PYBIND11_MODULE(dplvn, module)
 {
-    module.attr("__version__") = "2025.11.11a3";
+    module.attr("__version__") = "2025.11.11a4";
     module.doc() = 
         "Operator-splitting method of integrating DP-type Langevin equations"; 
   
